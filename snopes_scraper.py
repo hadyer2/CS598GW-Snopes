deb5112cb3f9299fe67e483bf6397a77763aee50
Hunter Dyer <hunter@Hunters-MacBook-Pro.local>
2020-10-10 15:29:54 -0500

    adding dataset

--- snopes_scraper.py
+++ snopes_scraper.py
@@ -30,35 +30,38 @@ for rating in ratings:
         soup = BeautifulSoup(url_content.content, 'html.parser')
         #check status code
         if url_content.status_code != 200:
-            print('Error on {}, status code of {}'.format(url, url_content.status_code)):
+            print('Error on {}, status code of {}'.format(url, url_content.status_code))
             page_valid = False
             break
 
         #Check if we can validly scrape page
         if page_valid:
             #extract list of articles on page
+            print(url)
             article_list = soup.findAll('div', {'class':'theme-content'})[0].findAll('article', {'class':'media-wrapper'})
 
             for article in article_list:
                 output_dictionary = {}
 
                 article_url = article.findAll('a', href = True)[0]['href']
-                output_dictionary['url'] = article_url
+                output_dictionary['url'] = str(article_url)
 
                 article_rating = rating
-                output_dictionary['rating'] = rating
+                output_dictionary['rating'] = str(rating)
 
                 title = article.findAll('h5', {'class':'title'})[0].contents
-                output_dictionary['title'] = title
+                output_dictionary['title'] = str(title)
 
-                subtitle = article.findAll('p',{'class':'subtitle'})[0].contents
-                output_dictionary['subtitle'] = subtitle
-
+                try:
+                    subtitle = article.findAll('p',{'class':'subtitle'})[0].contents
+                    output_dictionary['subtitle'] = str(subtitle)
+                except:
+                    output_dictionary['subtitle']=""
                 output_list.append(output_dictionary)
 
         page = page + 1
     output_string = json.dumps(output_list)
 
     #write out json string
-    with open('{}-stories.json','w') as fp:
+    with open('{}-stories.json'.format(rating),'w') as fp:
         fp.write(output_string)
